5e08571e78b968aed3d4b2a8c494618f8db83ad7
Theo Boschetti <boschetti.theo@gmail.com>
2019-10-18 10:01:26 +0200

    Creation of components repo and Game component

---
+++ frontend/src/Components/Game.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Map from "./Map";
+import "./Game.css";
+
+function Game() {
+  return (
+    <div className="gameScreen">
+      <div className="gameUI"></div>
+      <Map />
+    </div>
+  );
+}
+
+export default Game;
