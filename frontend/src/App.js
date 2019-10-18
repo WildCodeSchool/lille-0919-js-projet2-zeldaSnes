d76296e28ac9cb8c03c39caeda75971a296a3cb4
Theo Boschetti <boschetti.theo@gmail.com>
2019-10-18 10:04:46 +0200

    Game add to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Game from './Components/Game';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Game />
+  </div>;
 }
 
 export default App;
