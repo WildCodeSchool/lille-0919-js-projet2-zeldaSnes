bd08bc59e4c3fc95b858d164eec9954b7396a5ca
Theo Boschetti <boschetti.theo@gmail.com>
2019-10-23 12:06:16 +0200

    Rename of EmptyDiv component in LegtDiv

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Game from './Components/Game';
 import Title from './Components/Title';
-import EmptyDiv from './Components/EmptyDiv';
+import LeftDiv from './Components/LeftDiv';
 import ControlsTab from './Components/ControlsTab';
 import "./App.css";
 
@@ -9,7 +9,7 @@ function App() {
   return <div className="App">
     <Title />
     <div className="Container">
-      <EmptyDiv />
+      <LeftDiv />
       <Game />
       <ControlsTab />
     </div>
