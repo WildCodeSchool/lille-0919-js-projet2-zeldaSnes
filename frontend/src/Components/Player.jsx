f9d61c688d5b0891f2e6f8f18ddcd3b383c64a16
Gautier Mille <gautier.mille@gmail.com>
2019-10-23 15:25:27 +0200

    modification of player component with states

--- frontend/src/Components/Player.jsx
+++ frontend/src/Components/Player.jsx
@@ -4,29 +4,50 @@ import "./Player.css";
 // Affiche le player et calcul le mouvement a faire provenant de Map
 
 class Player extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      direction: "link/linkFront.png"
+    };
+  }
+
   // Fonction qui récupere l'input du clavier pour afficher le bon asset et faire bouger le player
-  getAsset() {
-    let direction;
-    switch (this.props.keyCode) {
+
+  getAsset(event) {
+    let newKeyCode = event.keyCode;
+    switch (newKeyCode) {
       case 37:
-        direction = "link/linkLeft.png";
+        this.setState({ direction: "link/linkLeft.png" });
         break;
 
       case 38:
-        direction = "link/linkBack.png";
+        this.setState({ direction: "link/linkBack.png" });
         break;
 
       case 39:
-        direction = "link/linkRight.png";
+        this.setState({ direction: "link/link-right-1-step.png" });
+        setTimeout(() => {
+          this.setState({ direction: "link/link-right-2-step.png" });
+        }, 40);
+        setTimeout(() => {
+          this.setState({ direction: "link/link-right-3-step.png" });
+        }, 40);
+        setTimeout(() => {
+          this.setState({ direction: "link/linkRight.png" });
+        }, 40);
         break;
 
       case 40:
-        direction = "link/linkFront.png";
+        this.setState({ direction: "link/linkFront.png" });
         break;
       default:
-        direction = "linkFront.png";
+        this.setState({ direction: "linkFront.png" });
     }
-    return direction;
+  }
+  componentDidMount() {
+    window.onkeydown = event => {
+      this.getAsset(event);
+    };
   }
 
   render() {
@@ -38,7 +59,7 @@ class Player extends React.Component {
           left: `${this.props.x * 32}px`
         }}
       >
-        <img src={this.getAsset()} alt={"Player"} />
+        <img src={this.state.direction} alt={"Player"} />
       </div>
     );
   }
