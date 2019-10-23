5dafdce8c47d030bac54439e95314b4d81af95f3
Theo Boschetti <boschetti.theo@gmail.com>
2019-10-23 15:17:53 +0200

    Implementation of a state equal of the height and width of the assets

--- frontend/src/Components/Player.jsx
+++ frontend/src/Components/Player.jsx
@@ -4,6 +4,13 @@ import "./Player.css";
 // Display the Player on the Map and get the movment from Map component
 
 class Player extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      assetHeight: 32,
+      assetWidth: 32
+    };
+  }
   //  Method which get inputs from ComponentDidMount (Game component) and send the correct asset to do on the Player
   getAsset() {
     let direction;
@@ -34,8 +41,8 @@ class Player extends React.Component {
       <div
         className="player"
         style={{
-          top: `${this.props.y * 32}px`,
-          left: `${this.props.x * 32}px`
+          top: `${this.props.y * this.state.assetHeight}px`,
+          left: `${this.props.x * this.state.assetWidth}px`
         }}
       >
         <img src={this.getAsset()} alt={"Player"} />
