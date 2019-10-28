6ce0ae59f5fdb5a2fe6f9e5209303371d23edabc
Gautier Mille <gautier.mille@gmail.com>
2019-10-28 09:57:20 +0100

    modify app and add homep page and gamepage components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import Gamepage from './Gamepage';
+import { Switch, Route } from 'react-router-dom';
+import Homepage from './Homepage'
 
 function App() {
-  return
-  <Switch>
-    <Route exact path='/' component={Hompegage} />
 
-  </Switch>
+  return (
+    <Switch>
+      <Route exact path='/' component={Homepage} />
+      <Route path='/Gamepage' component={Gamepage} />
+    </Switch>
+  )
 }
+
 
 export default App;
