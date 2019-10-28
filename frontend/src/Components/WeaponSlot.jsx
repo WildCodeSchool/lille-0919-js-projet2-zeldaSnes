49f1167e9cc47aa9c0059719058d96a47bacf9dc
Pauline Roche <rochepauline6@gmail.com>
2019-10-28 11:24:17 +0100

    adding WeaponSlot component to gameTopBar to display player's weapons and objects

---
+++ frontend/src/Components/WeaponSlot.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function WeaponSlot() {
+  return (
+    <div id="weaponSlot">
+      <div>
+        <p>X</p>
+        <p>W</p>
+        <p>C</p>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default WeaponSlot;
