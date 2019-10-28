8d534830b81930c5bc1e1bf2927417822027138f
Pauline Roche <rochepauline6@gmail.com>
2019-10-28 11:19:40 +0100

    adding gameTopBar component

---
+++ frontend/src/Components/GameTopBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import WeaponSlot from "./WeaponSlot";
+import RubySlot from "./RubySlot";
+import HealthBar from "./HealthBar";
+import "./GameTopBar.css";
+
+class GameTopBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="gameTopBar">
+        <HealthBar />
+
+        <WeaponSlot />
+
+        <RubySlot />
+      </div>
+    );
+  }
+}
+
+export default GameTopBar;
