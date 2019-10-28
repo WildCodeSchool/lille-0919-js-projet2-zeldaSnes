ab8e9e26ae3d4b9f3e315d960ecb3e3ae7281c99
Gumatan <arnaut.demaret@gmail.com>
2019-10-28 15:11:33 +0100

    modification des routes du switch dans App et css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './index.css';
-import Gamepage from './Gamepage';
-import { Switch, Route } from 'react-router-dom';
-import Homepage from './Homepage'
+import React from "react";
+import "./index.css";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./Components/HomePage";
+import GamePage from "./Components/GamePage";
 
 function App() {
-
   return (
     <Switch>
-      <Route exact path='/' component={Homepage} />
-      <Route path='/Gamepage' component={Gamepage} />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/Gamepage" component={GamePage} />
+      <Route path="/About" component={HomePage} />
+      <Route path="/Timeline" component={HomePage} />
     </Switch>
-  )
+  );
 }
-
 
 export default App;
