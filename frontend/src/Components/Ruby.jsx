8e23c251ee592f8d8e7044e74629437575e0b52c
Bastien Saulnier <bastien.saul@gmail.com>
2019-10-28 09:20:45 +0100

    Creation of the Ruby component

---
+++ frontend/src/Components/Ruby.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Ruby.css";
+
+class Ruby extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      assetHeight: 32,
+      assetWidth: 32
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          className="Ruby"
+          style={{
+            top: `${this.props.yRuby * this.state.assetHeight}px`,
+            left: `${this.props.xRuby * this.state.assetWidth}px`
+          }}
+        ></div>
+      </div>
+    );
+  }
+}
+export default Ruby;
