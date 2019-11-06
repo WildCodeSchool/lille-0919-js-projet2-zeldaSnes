d64e77b2cb1ac10bfbf5f6a117a1f9778c6444fc
Gumatan <arnaut.demaret@gmail.com>
2019-11-06 11:12:23 +0100

    removed a useless constructor in HealthBar.jsx

--- frontend/src/Components/HealthBar.jsx
+++ frontend/src/Components/HealthBar.jsx
@@ -3,10 +3,6 @@ import React from "react";
 let heartsTab = ["fullHeart", "fullHeart", "fullHeart", "halfHeart"];
 
 class HealthBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div id="healthBar">
