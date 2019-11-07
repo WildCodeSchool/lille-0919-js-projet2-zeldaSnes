307b7db5c43bb90db132641eb4399af198481e0d
Pauline Roche <rochepauline6@gmail.com>
2019-11-07 15:56:33 +0100

    axios request added to timeline component and response filtered and sorted

--- frontend/src/Components/Timeline.jsx
+++ frontend/src/Components/Timeline.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 class TimeLine extends React.Component {
   constructor(props) {
@@ -7,6 +8,53 @@ class TimeLine extends React.Component {
     this.state = {
       zeldaData: null
     };
+  }
+  excludeList = [
+    "3030-53074",
+    "3030-49994",
+    "3030-44782",
+    "3030-37074",
+    "3030-17713",
+    "3030-14181",
+    "3030-22245",
+    "3030-22297",
+    "3030-8881",
+    "3030-35064",
+    "3030-17278",
+    "3030-37074",
+    "3030-51484",
+    "3030-72586",
+    "3030-14114"
+  ];
+  componentDidMount() {
+    this.getZeldaData();
+  }
+
+  getZeldaData() {
+    const url =
+      "https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/";
+    axios
+      .get(
+        url +
+          "games/?api_key=c887622ccf98222dd8620e272f451137cbfa298d&format=json&limit=40&filter=name:zelda"
+      )
+      .then(response => {
+        let tabData = response.data.results;
+        tabData = tabData.filter(game => {
+          return (
+            (game.expected_release_year !== null ||
+              game.expected_release_month !== null) &&
+            !this.excludeList.includes(game.guid)
+          );
+        });
+        tabData.sort(function(a, b) {
+          return a.expected_release_year - b.expected_release_year;
+        });
+        console.log(tabData);
+        this.setState({
+          zeldaData: tabData
+        });
+      });
   }
 
   render() {
