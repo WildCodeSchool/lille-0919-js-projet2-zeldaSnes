d3751f6c8e12c8f0be346752c856ac29b9511e5c
Gautier Mille <gautier.mille@gmail.com>
2019-11-13 17:18:52 +0100

    delete console log

--- frontend/src/Components/Game.jsx
+++ frontend/src/Components/Game.jsx
@@ -471,7 +471,6 @@ class Game extends React.Component {
         newRubyList[i].y === yPlayer &&
         newRubyList[i].rubyMap === this.state.mapNumber
       ) {
-        console.log(newRubyList);
         newRubyList[i].rubyClass = "RubyTaken";
         this.setState({ rubyList: newRubyList });
         setTimeout(() => {
