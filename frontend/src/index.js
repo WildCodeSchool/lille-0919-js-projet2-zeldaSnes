4d6851c46b31cac8e43915b463848c046a6c1fda
david derancourt <david.derancourt@hotmail.fr>
2019-11-21 16:43:36 +0100

    creation of the store in reducer, dispatch of the store in app component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App /></BrowserRouter>, document.getElementById('root'));
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./store/reducer";
 
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
